998ced7b056d6810598376c15be118a18e814e18
Arik Westbrook <awestbrook@technavigators.dev>
2023-07-27 22:47:57 -0700

    Modified flite_main.c to replace the main method with a minimal set of functions suitable for exporting with emscripten for use in the browser.

---
+++ build_flite.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+export set LDFLAGS='-s INITIAL_MEMORY=128MB'
+export set CFLAGS='-s EXPORTED_RUNTIME_METHODS=ccall -s EXPORT_ES6 -s MODULARIZE -s EXPORT_NAME=flite -s EXPORT_ALL -s LINKABLE'
+
+echo "
+#
+#
+#  The base languages, lexicons and voices
+LEXES += cmulex
+LANGS += usenglish
+VOXES += cmu_us_rms
+" > config/default.lv
+
+emconfigure ./configure --host=wasm32-wasi
+emmake make clean
+emmake make
+
+cp bin/flite ~/test/flite.mjs
+cp bin/flite.wasm ~/test
